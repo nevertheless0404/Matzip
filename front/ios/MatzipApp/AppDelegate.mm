8545c4d5178440d5f8a615298a08f6bf65ac8619
nevertheless0404 <nevertheless.hi@gmail.com>
2025-02-04 16:02:18 +0900

    Google map error

--- front/ios/MatzipApp/AppDelegate.mm
+++ front/ios/MatzipApp/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyBZCg6_kw47T0sYB8jzo_aRF0Fy-P7GwC4"];
   self.moduleName = @"MatzipApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import useAuth from '@/hooks/queries/useAuth';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 function MapHomeScreen() {
   const {logoutMutation} = useAuth();
 
-  return (
-    <SafeAreaView>
-      <Text>맵 스크린</Text>
-      <Button title="로그아웃" onPress={() => logoutMutation.mutate(null)} />
-    </SafeAreaView>
-  );
+  const handleLogout = () => {
+    logoutMutation.mutate(null);
+  };
+
+  return <MapView style={styles.container} provider={PROVIDER_GOOGLE}></MapView>
+
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+});
 
 export default MapHomeScreen;
